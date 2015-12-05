acca287aea24cde360a3a1e8529ca8e13bab560b
Simeon Andreev <simo1914@abv.bg>
2015-12-05 14:01:38 +0200

    Second commit

--- Space Invaders/GameScene.m
+++ Space Invaders/GameScene.m
@@ -8,18 +8,54 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
+@implementation GameScene {
+
+SKNode *_mainLayer;
+SKSpriteNode *_cannon;
+}
+
+static inline CGVector radiansToVector(CGFloat radians)
+{
+    CGVector vector;
+    vector.dx = cosf(radians);
+    vector.dy = sinf(radians);
+    return vector;
+}
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+   
+    // Add background.
+    SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"Starfield"];
+    background.size = self.frame.size;
+    background.position = CGPointZero;
+    background.anchorPoint = CGPointZero;
+    background.blendMode = SKBlendModeReplace;
+    [self addChild:background];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 45;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    // Add main layer.
+    _mainLayer = [[SKNode alloc] init];
+    [self addChild:_mainLayer];
     
-    [self addChild:myLabel];
+    // Add cannon.
+    _cannon = [SKSpriteNode spriteNodeWithImageNamed:@"Cannon"];
+    _cannon.position = CGPointMake(self.size.width * 0.5, 0.0);
+    [_mainLayer addChild:_cannon];
+    
+    // Create cannon rotation actions.
+    SKAction *rotateCannon = [SKAction sequence:@[[SKAction rotateByAngle:M_PI duration:2],
+                                                  [SKAction rotateByAngle:-M_PI duration:2]]];
+    [_cannon runAction:[SKAction repeatActionForever:rotateCannon]];
+}
+
+-(void)shoot
+{
+    // Create ball node.
+    SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"Ball"];
+    CGVector rotationVector = radiansToVector(_cannon.zRotation);
+    ball.position = CGPointMake(_cannon.position.x + (_cannon.size.width   * rotationVector.dx),
+                                _cannon.position.y + (_cannon.size.width * 0.5 * rotationVector.dy));
+    [_mainLayer addChild:ball];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -27,18 +63,18 @@
     
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+        [self shoot];
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
     }
 }
 
